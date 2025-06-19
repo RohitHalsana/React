c1dc7a4c65a71cf6e8e5f1b5a4e91051264de9aa
RohitHalsana <rohithalsana08@gmail.com>
2025-06-19 17:13:05 +0530

    Change Background

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 function App() {
   const [color, setColor] = useState("green")
-
+  
   return (
     <div className="w-full h-screen duration-300"
     style ={{ backgroundColor: color }} >
