be8ec571c813d322b811b3a07d4f480930617108
RohitHalsana <rohithalsana08@gmail.com>
2025-06-29 21:24:51 +0530

    bg changer

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 function App() {
   const [color, setColor] = useState("green")  
   return (
-    <div className="w-full h-screen duration-300"
+    <div className="w-full min-h-screen duration-300"
     style ={{ backgroundColor: color }} >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl ">
